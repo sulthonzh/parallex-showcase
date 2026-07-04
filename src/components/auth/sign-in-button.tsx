f588488cc26f3e3125ce3db0d387e2e464accfa4
dev <dev@local>
2026-07-04 15:14:54 +0700

    feat: add Google sign-in button

--- src/components/auth/sign-in-button.tsx
+++ src/components/auth/sign-in-button.tsx
@@ -1,17 +1,31 @@
 import { signIn } from "@/auth";
 import { Button } from "@/components/ui/button";
 
-export function SignInButton() {
+export function SignInButtons() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github", { redirectTo: "/" });
-      }}
-    >
-      <Button type="submit" className="w-full">
-        Sign in with GitHub
-      </Button>
-    </form>
+    <div className="flex flex-col gap-3">
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github", { redirectTo: "/" });
+        }}
+      >
+        <Button type="submit" className="w-full">
+          Sign in with GitHub
+        </Button>
+      </form>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google", { redirectTo: "/" });
+        }}
+      >
+        <Button type="submit" variant="outline" className="w-full">
+          Sign in with Google
+        </Button>
+      </form>
+    </div>
   );
 }
+
+export { SignInButtons as SignInButton };
